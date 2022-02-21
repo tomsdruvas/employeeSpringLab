ba8d49aede09bf55436130cac581da5df00fa8df
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-02-21 14:58:17 +0000

    made employee class

--- src/main/java/com/codeclan/example/employeeTrackingApplication/controllers/EmployeeController.java
+++ src/main/java/com/codeclan/example/employeeTrackingApplication/controllers/EmployeeController.java
@@ -23,6 +23,6 @@ public class EmployeeController {
 
     @GetMapping(value = "/employees/{id}")
     public Optional<Employee> getEmployee(@PathVariable Long id){
-        return employeeRepository.findById(id);
+        return employeeRepository.findById(id)
     }
 }
